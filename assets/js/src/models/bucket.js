462b9350498341f6d9950698f0c107e8afdef425
Seth Benjamin <animecyc@gmail.com>
2015-03-10 22:44:45 -0400

    Cleaning up for-loop block

--- assets/js/src/models/bucket.js
+++ assets/js/src/models/bucket.js
@@ -8,10 +8,7 @@ define(['models/item'], function(Item) {
     this.items = [];
 
     for(var idx in repo.items) {
-      var item = repo.items[idx];
-      var itemMeta = definitions.items[item.itemHash];
-
-      this.items.push(new Item(definitions, itemMeta));
+      this.items.push(new Item(definitions, repo.items[idx]));
     }
   }
 
