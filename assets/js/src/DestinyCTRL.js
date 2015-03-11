356e89ab5458ad511400ea0d2ab14918dfe0d202
Seth Benjamin <animecyc@gmail.com>
2015-03-11 17:00:19 -0400

    Just messing around for now, jQuery? Why not, i really dont want to use it .... MithrilJS anyone?

--- assets/js/src/DestinyCTRL.js
+++ assets/js/src/DestinyCTRL.js
@@ -6,6 +6,29 @@ define(['common/bungie'], function(Bungie) {
 
     Bungie.authorize().then(function() {
       // Do something awesome.... or not... whatever
+
+      var accounts = Bungie.getAccounts();
+
+      if(accounts.length) {
+        accounts[0].getVault().then(function(vault) {
+          var $vault = $('#vault');
+
+          vault.getAll().forEach(function(bucket) {
+            var $bucket = $('<ul id="' + bucket.type + '" />');
+
+            bucket.getItems().forEach(function(item) {
+              var $item = $('<li />');
+              var $icon = $('<img />')
+                .attr('src', item.icon);
+
+              $item.append($icon);
+              $bucket.append($item);
+            });
+
+            $vault.append($bucket);
+          });
+        });
+      }
     }).catch(function(err) {
       alert(err.Message);
     });
