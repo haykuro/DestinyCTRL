db27f4949aab37a27ff85a1f19afdd09c6fce088
Seth Benjamin <animecyc@gmail.com>
2015-03-10 23:49:16 -0400

    Add getters for specific bucket types

--- assets/js/src/models/vault.js
+++ assets/js/src/models/vault.js
@@ -7,5 +7,39 @@ define(['models/bucket'], function(Bucket) {
     });
   }
 
+  Vault.prototype._getBucketByType = function(type) {
+    var _bucket = null;
+
+    this.buckets.some(function(bucket) {
+      if(bucket.type === type) {
+        _bucket = bucket;
+
+        return true;
+      }
+
+      return false;
+    });
+
+    return _bucket;
+  };
+
+  Vault.prototype.getAll = function() {
+    return this.buckets.reduce(function(memo, bucket) {
+      return memo.concat([bucket]);
+    }, []);
+  };
+
+  Vault.prototype.getGeneral = function() {
+    return this._getBucketByType('BUCKET_VAULT_ITEMS');
+  };
+
+  Vault.prototype.getWeapons = function() {
+    return this._getBucketByType('BUCKET_VAULT_WEAPONS');
+  };
+
+  Vault.prototype.getArmor = function() {
+    return this._getBucketByType('BUCKET_VAULT_ARMOR');
+  };
+
   return Vault;
 });
