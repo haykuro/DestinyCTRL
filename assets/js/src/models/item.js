1e76a709960b68f25fcffc6a4fca1da5dff7c22b
Seth Benjamin <animecyc@gmail.com>
2015-03-10 22:46:26 -0400

    Large cleanup of model data; Stage support for item talent grids

--- assets/js/src/models/item.js
+++ assets/js/src/models/item.js
@@ -9,35 +9,94 @@ define(['models/bucket'], function(Bucket) {
   }
 
   function Item(definitions, repo) {
+    var meta = definitions.items[repo.itemHash];
+
     this.id = repo.itemHash;
-    this.name = repo.itemName;
-    this.type = repo.itemType;
-    this.typeName = repo.itemType;
-    this.description = repo.itemDescription;
-    this.icon = 'https://www.bungie.net/' + repo.icon.replace(/^\//, '');
+    this.name = meta.itemName;
+    this.type = meta.itemType;
+    this.typeName = meta.itemType;
+    this.description = meta.itemDescription;
+    this.icon = 'https://www.bungie.net/' + meta.icon.replace(/^\//, '');
     this.stats = {};
     this.primaryStatId = null;
-    this.talentGrid = definitions.talentGrids[repo.talentGridHash];
-    this.tier = {
-      type : repo.tierType,
-      name : repo.tierName
-    };
+    this.talentGrid = [];
+    this.tier = { type : meta.tierType, name : meta.tierName };
+    this._definitions = definitions;
 
-    for(var idx in repo.baseStats) {
-      var baseStat = repo.baseStats[idx];
-      var baseStatMeta = definitions.stats[baseStat.statHash];
+    this._fillBaseStats(repo.stats);
+    this._fillPrimaryStat(meta.primaryBaseStat);
+    this._fillTalentGrid(
+      repo.nodes,
+      definitions.talentGrids[meta.talentGridHash]
+    );
+  }
 
-      this.stats[baseStatMeta.statIdentifier] =
-        createStat(baseStat, baseStatMeta);
-    }
+  Item.prototype._fillBaseStats = function(stats) {
+    stats.forEach(function(stat) {
+      var meta = this._definitions.stats[stat.statHash];
 
-    var primaryBaseStat = repo.primaryBaseStat;
-    var primaryBaseStatMeta = definitions.stats[primaryBaseStat.statHash];
+      this.stats[meta.statIdentifier] = createStat(stat, meta);
+    }, this);
+  };
 
-    this.primaryStatId = primaryBaseStatMeta.statIdentifier;
+  Item.prototype._fillPrimaryStat = function(stat) {
+    var meta = this._definitions.stats[stat.statHash];
 
-    this.stats[this.primaryStatId] =
-      createStat(primaryBaseStat, primaryBaseStatMeta);
+    this.primaryStatId = meta.statIdentifier;
+
+    this.stats[this.primaryStatId] = createStat(stat, meta);
+  };
+
+  Item.prototype._fillTalentGrid = function(itemNodes, talentGrid) {
+    itemNodes.map(function(node, idx) {
+      // The talentGrid maps 1:1 to an
+      // item's "nodes"; Thanks for the confusing
+      // definitions Bungie!
+
+      var talentNode = talentGrid.nodes[idx];
+      var step = talentNode.steps[node.stepIndex];
+
+      return {
+        name : step.nodeStepName,
+        description : step.nodeStepDescription,
+        icon : 'https://www.bungie.net/' + step.icon.replace(/^\//, ''),
+        requirements : step.activationRequirement,
+
+        // We need the row-column data from the
+        // talentGridNode to properly build our
+        // items progression matrix, order isn't
+        // important (we sort that out later)
+
+        row : talentNode.row,
+        column : talentNode.column
+      };
+    }).forEach(function(node) {
+      // We only want rows and columns that
+      // are greater than 1, from what I can
+      // tell anything but that isn't used
+
+      if(node.row > -1 && node.column > -1) {
+        // Build our column-row matrix, again,
+        // not worrying about sort order as we
+        // will handle that later
+
+        if(! this.talentGrid.hasOwnProperty(node.column)) {
+          this.talentGrid[node.column] = [];
+        }
+
+        this.talentGrid[node.column].push(node);
+      }
+    }, this);
+
+    // Sort the row columns just in case
+    // we have them in the wrong order from
+    // the previous operations
+
+    this.talentGrid.forEach(function(column) {
+      column.sort(function(a, b) {
+        return a.row - b.row;
+      });
+    });
   }
 
   return Item;
