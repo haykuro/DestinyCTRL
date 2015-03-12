78f156c277eaa49c45f33e9cba97f7bd425bb31c
Seth Benjamin <animecyc@gmail.com>
2015-03-12 14:00:43 -0400

    Add support for class extensions

--- assets/js/src/common/utils.js
+++ assets/js/src/common/utils.js
@@ -36,5 +36,25 @@ define(function() {
     };
   };
 
+  Util.inheritClass = function (subClass, superClass) {
+    if (typeof superClass !== 'function' && superClass !== null) {
+      throw new TypeError('Super expression must either be null or' +
+        ' a function, not ' + typeof superClass);
+    }
+
+    subClass.prototype = Object.create(superClass && superClass.prototype, {
+      constructor: {
+        value : subClass,
+        enumerable : false,
+        writable : true,
+        configurable : true
+      }
+    });
+
+    if (superClass) {
+      subClass.__proto__ = superClass;
+    };
+  }
+
   return Util;
 });
