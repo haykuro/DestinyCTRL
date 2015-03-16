621a97c70793d69dfc222c5dfb78088237fc0f22
Seth Benjamin <animecyc@gmail.com>
2015-03-16 13:28:20 -0400

    Added GPLv3 license

--- libs/background.js
+++ libs/background.js
@@ -1,3 +1,24 @@
+/**
+ * Asset and logitics manager for Bungie's Destiny
+ *
+ * Copyright (c) 2015 DestinyCTRL
+ *
+ * Seth Benjamin <animecyc@gmail.com>
+ * Arissa Brown <flipmodes01@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 (function() {
   'use strict';
 
